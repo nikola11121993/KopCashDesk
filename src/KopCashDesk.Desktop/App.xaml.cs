ea8aac0dc89faf934bad4a0bfaee7f4638810209
nikola11121993 <nikola11121993@gmail.com>
2026-09-08 13:30:32 +0500

    feat: initialize desktop application and error handling

---
+++ src/KopCashDesk.Desktop/App.xaml.cs
@@ -0,0 +1,18 @@
+using System.Windows;
+using System.Windows.Threading;
+
+namespace KopCashDesk.Desktop;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        DispatcherUnhandledException += OnUnhandledException;
+    }
+    private static void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        MessageBox.Show("Произошла ошибка. Данные не будут изменены.\n\n" + e.Exception.Message, "КОП Кассы", MessageBoxButton.OK, MessageBoxImage.Error);
+        e.Handled = true;
+    }
+}
