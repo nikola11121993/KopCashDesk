46ff983a6f403fc1b632749c66909ffa55929e68
nikola11121993 <nikola11121993@gmail.com>
2026-09-11 13:46:45 +0500

    feat: expose Frontol import from main window

--- src/KopCashDesk.Desktop/MainWindow.FiscalImport.cs
+++ src/KopCashDesk.Desktop/MainWindow.FiscalImport.cs
@@ -14,4 +14,15 @@ public partial class MainWindow
         }) { Owner = this };
         window.ShowDialog();
     }
+
+    private void FrontolReportImport_Click(object sender, RoutedEventArgs e)
+    {
+        var window = new FrontolReportImportWindow(_db, SelectedOrganizationId, () =>
+        {
+            RefreshAll();
+            StatusText.Text = "Выгрузка Frontol 6 обработана";
+            if (_page == "summary") PageContent.Content = RenderSummary();
+        }) { Owner = this };
+        window.ShowDialog();
+    }
 }
