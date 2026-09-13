70401b0f52fbf0b1fa074e9ea9bf02007ff423c3
nikola11121993 <nikola11121993@gmail.com>
2026-09-13 15:42:24 +0500

    feat: use assembly metadata as application version source

---
+++ src/KopCashDesk.Desktop/AppVersion.cs
@@ -0,0 +1,30 @@
+using System.Reflection;
+using System.Windows;
+
+namespace KopCashDesk.Desktop;
+
+internal static class AppVersion
+{
+    public static string Current
+    {
+        get
+        {
+            var informational = Assembly.GetExecutingAssembly()
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion;
+            if (!string.IsNullOrWhiteSpace(informational))
+                return informational.Split('+', 2)[0];
+
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            return version is null ? "0.0.0" : $"{version.Major}.{version.Minor}.{version.Build}";
+        }
+    }
+
+    public static string Display => "v" + Current;
+}
+
+public partial class MainWindow
+{
+    private void AboutVNext_Click(object sender, RoutedEventArgs e) =>
+        MessageBox.Show(this, $"КОП Кассы {AppVersion.Current}\nC# / .NET 10 / WPF", "О программе");
+}
